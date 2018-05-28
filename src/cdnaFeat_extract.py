33ccd8c3841c0c93906f8f512aa526fa597f5cb9
Arnold <aerod7@hotmail.fr>
2018-05-28 13:33:05 +0200

    maj cdnafeat_extract

--- src/cdnaFeat_extract.py
+++ src/cdnaFeat_extract.py
@@ -12,8 +12,10 @@
 from Bio import SeqIO
 import pandas as pd
 import numpy as np
+import sys
 import argparse
 
+'''
 #Options/Arguments parser
 parser = argparse.ArgumentParser(description = "Getting Features from a cDNAtable")
 #Positionnal Arguments
@@ -24,18 +26,16 @@ parser.add_argument('outfile', nargs = "?", default = sys.stdout, metavar = "out
 
 
 args = parser.parse_args()
+'''
 
 list_param = []
 list_seq = []
-with open(args.infile, "rU") as handle:
-	i=0
+with open("../data/BertrandandRandom_Mart_Export.txt", "rU") as handle:
 	for record in SeqIO.parse(handle, "fasta"):
 		if(len(record.id.split("|"))==13):
 			list_param.append(str(record.id))
 			list_seq.append(str(record.seq))
-		i+=1
-		if i==5:
-			break
+		
 
 cdna_seq = pd.Series(list_seq)
 ensembl_gene_id = []
@@ -88,7 +88,7 @@ cdna_feat.columns=["cdna_seq", "ensembl_gene_id", "ensembl_transcript_id", "exte
  "transcript_end", "utr5_start", "utr5_end", "utr3_start", "utr3_end", "transcription_start_site", "transcript_biotype",
   "cdna_coding_start", "cdna_coding_end"]
 
-
+print cdna_feat.shape
 
 def get_utr5MAX(cdna_feat_row):
 	print("getting UtrMax...")
@@ -173,5 +173,41 @@ for i in range(cdna_feat.shape[0]):
 cdna_feat = cdna_feat.replace('',np.NaN)
 cdna_feat = cdna_feat.dropna()
 
-#Sauvegarde cdnaFeature_Table
-cdna_feat.to_csv(args.outfile, sep = "\t", index = False)
+
+cdna_feat = cdna_feat.reset_index()
+
+
+
+tmp =cdna_feat.loc[:,['transcript_start', 'transcript_end']].astype(int)
+
+trlength = pd.DataFrame(tmp.transcript_end-tmp.transcript_start,
+ columns=["trlength"])
+
+tmp2 = pd.DataFrame()
+tmp2[0] = cdna_feat['ensembl_gene_id']
+tmp2[1] = trlength
+tmp2.columns= ['ensembl_gene_id', 'trlength']
+
+
+new_tab = pd.DataFrame()
+for geneid in np.unique(tmp2.ensembl_gene_id):
+	ok1 = tmp2[tmp2.ensembl_gene_id==geneid]
+	if(ok1.shape[0]==1):
+		new_tab=pd.concat([new_tab,ok1])
+		tmp2 = tmp2.drop(ok1.index)
+	elif(ok1.shape[0]>1):
+		ok2 = ok1[ok1.trlength==max(ok1.trlength)]
+		if(ok2.shape[0]==1):
+			new_tab=pd.concat([new_tab,ok2])
+		else:
+			new_tab=pd.concat([new_tab,ok2.iloc[0:1,:]])
+		tmp2 = tmp2.drop(ok1.index)
+
+	
+
+cdna_feat = cdna_feat.iloc[new_tab.index,:].reset_index().iloc[:,2:]
+
+print cdna_feat
+
+cdna_feat.to_csv("BertrandandRandom_Mart_Export_EXTRACT.txt",
+ sep= "\t", index=False)
