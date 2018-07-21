1639b4733b690e1ee60a332f651b375e42ddff42
antoinezl <antoinezl@users.noreply.github.com>
2018-07-21 09:25:31 +0200

    ADD geneIDs2fasta.py script

---
+++ mouse/src/geneIDs2fasta.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 *-*
+
+def geneIDs2Fasta(id_list, out_fasta):
+    #Loading packages
+    import numpy as np
+    import pandas as pd
+    import math
+    import re
+    import sys
+    import subprocess
+    import rnaFeaturesLib
+    import argparse
+    from biomart import BiomartServer
+
+    server = BiomartServer( "http://www.ensembl.org/biomart/")
+    server.show_datasets()
+
+    dt = server.datasets['mmusculus_gene_ensembl']
+    # remplacer par 'hsapiens_gene_ensembl'
+
+    # run a search with custom filters and attributes (no header)
+    # 3Plen, 3PEng, 5Plen, 5PEng, 3PNorm, 5PNorm, dORF, uORF, KozakSeq, KozakCont
+
+    with open(id_list, "r") as filin:
+        listID = filin.read().splitlines()
+
+    filout = open("tmp", "w")
+
+    response = dt.search({
+    'filters': {
+        'ensembl_gene_id': listID
+    },
+    'attributes': [
+        'ensembl_gene_id', 'ensembl_transcript_id','external_gene_name','transcript_start', 
+        'transcript_end', '5_utr_end', '5_utr_start', '3_utr_end', '3_utr_start',
+        'transcription_start_site', 'transcript_biotype', 'cdna_coding_start',
+        'cdna_coding_end','cdna'
+    ]
+    }, header = 1 )
+
+    filout.write(response.text)
+    filout.close()
+
+    # reading data
+    cdna = pd.read_csv("tmp.tsv", sep="\t")
+
+    # remove NA and reset index
+    cdna = cdna.dropna()
+    cdna = cdna.reset_index()
+
+    # select only longest 5'UTR and 3'UTR
+    for i in range(cdna.shape[0]):
+        ligne = pd.DataFrame(cdna.loc[i,:]).transpose()
+        #For 5_UTR_start
+        indices = get_utr5MAX(ligne)
+        if(len(cdna.iloc[i:i+1,:]["5' UTR start"].values[0].split(";")) > 1):
+            cdna.iloc[i:i+1,7:8] = indices[1]
+            cdna.iloc[i:i+1,8:9] = indices[0]
+        if(len(cdna.iloc[i:i+1,:]["3' UTR start"].values[0].split(";")) > 1):
+            cdna.iloc[i:i+1,9:10] = indices[1]
+            cdna.iloc[i:i+1,10:11] = indices[0]
+
+    # Features calculations:
+
+    if __name__ == '__main__':
+
+    program = 'geneIDs2Fasta'
+    version = 0.1
+    description = 'Fetch features for an ensembl gene ID list and output a multi FASTA file'
+    parser = argparse.ArgumentParser(prog=program)
+    parser = argparse.ArgumentParser(description=description, epilog="Author: Franz-Arnold Ake and Antoine Lu, 2018")
+    parser.add_argument('--version', action='version', version='{} {}'.format(program, version))
+    parser.add_argument("i", help="input id file", type=str)
+    parser.add_argument("o", help="output file name", type=int)
+    parser.add_argument("overlap", help="overlap between window", type=int)
+    parser.add_argument("exportR", help="To export data in formated R text", type=bool, choices=[True, False])
+
+    args = parser.parse_args()
+
